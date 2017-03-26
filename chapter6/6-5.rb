566b6f64397115023830799961742c6891ed90b2
Bailey Kursar <bkursar@gmail.com>
2017-03-26 22:46:29 +0100

    Adding Chapter 6

---
+++ chapter6/6-5.rb
@@ -0,0 +1,17 @@
+puts rand
+
+puts rand(11)
+## A random number out of 10 requires the upper limit to be 11 ##
+
+puts rand(101)
+
+srand 29
+puts (rand(30))
+puts (rand(30))
+puts (rand(30))
+puts ""
+srand 29
+puts (rand(30))
+puts (rand(30))
+puts (rand(30))
+
---
+++ chapter6/6-6.rb
@@ -0,0 +1,3 @@
+puts(Math::PI)
+
+
