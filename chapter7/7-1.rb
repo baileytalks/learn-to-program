5859724cf2a78ec72194ee380754d12d7333ea6e
Bailey Kursar <bkursar@gmail.com>
2017-03-27 00:22:10 +0100

    Adding Chapter 7

---
+++ chapter7/7-1.rb
@@ -0,0 +1,36 @@
+puts 5 > 2
+puts 7 > 9
+
+puts ""
+
+puts 5 >= 2
+puts 7 >= 9
+puts 7 >= 7
+
+puts ""
+
+## == means "are these equal?"
+
+puts "5 == 5"
+puts 5 == 5
+puts "5 == 6"
+puts 5 == 6
+
+puts ""
+
+puts "5 != 5"
+puts 5 != 5
+puts "5 != 6"
+puts 5 != 6
+
+
+puts ""
+
+## comparing letters in strings will compare the first character of the string alphabetically, and with upper case before lower case. UPPER > lower :)
+
+puts "ant < dec"
+puts "ant" < "dec"
+puts "cheese > mouse"
+puts "cheese" > "mouse"
+puts "cheese > Mouse"
+puts "cheese" > "Mouse"
